b27c91ab405a95ecf8476efe994b5a0af95cee37
Fulei Huang <huangfulei1027@gmail.com>
2019-05-04 23:55:30 +0100

    removed not needed import

--- src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/HelloWorldController.java
@@ -1,10 +1,7 @@
 package com.aws.codestar.projecttemplates.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Basic Spring MVC controller that handles all GET requests.
